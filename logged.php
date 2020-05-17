77ae19b58c5b1b312d121ff3bda96a72f22127c5
Sebastian <65468884+dynamiccodePL@users.noreply.github.com>
2020-05-17 02:01:44 +0200

    Create logged.php

---
+++ logged.php
@@ -0,0 +1,41 @@
+/*
+* Autor: https://programistyczny.blogspot.com
+*/
+
+<?php
+
+ session_start();
+ 
+ if(!isset($_SESSION['logged'])) {
+ 
+  header('Location: login.php');
+  
+  exit;
+  
+ }
+ 
+?>
+
+<!DOCTYPE html>
+<html>
+ <head>
+  <meta charset="utf-8" />
+  
+  <title>Zalogowany</title>
+  
+ </head>
+ <body>
+  <div>
+  <?php
+  
+  if(isset($_SESSION['logged'])):
+  
+  ?>
+  
+  <p>Jesteś zalogowany jako <strong><?= $_SESSION['username']; ?></strong>.</p>
+  <p><a href="logout.php">Wyloguj się</a></p>
+  
+  <?php endif; ?>
+  </div>
+ </body>
+</html>
